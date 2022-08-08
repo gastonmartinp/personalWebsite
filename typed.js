f18ea4a94c9db2921cac1403a48f62d24d171d71
gastonmartinp <gas.perez96@gmail.com>
2022-08-07 22:58:00 -0300

    add an animation in js

--- typed.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed('.typed-words', {
-    strings: ["Product Owner", "Product Manager"],
-    typeSpeed: 80,
-    backSpeed: 80,
-    backDelay: 4000,
-    startDelay: 1000,
-    loop: true,
-    showCursor: true
-});
